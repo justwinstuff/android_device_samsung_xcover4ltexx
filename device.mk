5656d3199f623cd384229b8d31359aa0d0e71d92
WinStuff <104269577+justwinstuff@users.noreply.github.com>
2026-03-28 21:01:59 +0200

    update
    Change-Id: Ice2e8829b5f52c1f0486c5275ce387a125959211

--- device.mk
+++ device.mk
@@ -1,3 +1,15 @@
 # SPDX-License-Identifier: Apache-2.0
 
-# DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+# Inherit from common samsung exynos7570-common
+$(call inherit-product, device/samsung/universal7570-common/device-common.mk)
+
+# Set the screen density (adjust if 320 is wrong for xcover4)
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+
+# Hardware features
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml
+
+# Call the proprietary blobs (This is usually where your build.prop content comes from)
+$(call inherit-product, vendor/samsung/xcover4ltexx/xcover4ltexx-vendor.mk)
